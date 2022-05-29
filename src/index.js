0e2b0232f5e75a09d9247778160db7af05ca4fb8
apodavonov3616 <apodavonov3616@DESKTOP-BTS3T0E.localdomain>
2022-05-29 14:08:57 -0400

    minor changes

--- src/index.js
+++ src/index.js
@@ -4,24 +4,28 @@ import Bench from "./scripts/bench"
 import Deadlift from "./scripts/deadlift"
 
 console.log("webpack is working")
+console.log(d3)
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const canvasEl = document.getElementById("main-canvas");
-    canvasEl.width = 1300;
-    canvasEl.height = 1000;
 
-    const ctx = canvasEl.getContext("2d");
+    // const canvasEl = document.getElementById("main-canvas");
+    // canvasEl.width = 1300;
+    // canvasEl.height = 1000;
+
+    // const ctx = canvasEl.getContext("2d");
+
+
     // ctx.fillStyle = "grey";
     // ctx.fillRect(0, 0, 1300, 1000);
 
     // const main = document.getElementById("main")
     // new Bench(main)
-    let img = new Image();
-    img.src = "../src/images/00001.png"
-    img.onload = function () {
-        ctx.drawImage(img, 240, 240)
-    }
+    // let img = new Image();
+    // img.src = "../src/images/squat/00001.png"
+    // img.onload = function () {
+    //     ctx.drawImage(img, 240, 240)
+    // }
 
 
     // document.getElementById("main-canvas").innerHTML='<img src="../src/images/strick_figure.png" alt="sample"'>;
